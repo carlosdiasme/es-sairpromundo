cea5c5d9e836a01fd6b25905336a3fd106afbd8c
Carlos Dias <carlosdias.me@gmail.com>
2024-11-20 17:17:17 -0300

    fix

--- components/nav/NavPlace.tsx
+++ components/nav/NavPlace.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import Link from "next/link"
-import { MapPin, AudioWaveform, ListFilter, Heart, ChevronDown } from 'lucide-react'
+import { MapPin, AudioWaveform, ListFilter, ChevronDown } from 'lucide-react'
 
 import {
   Dialog,
